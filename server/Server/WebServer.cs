7c94f4c5b5bef4d28a1ad5eae9f6b177ecbc0917
oclin <contact@monroeclinton.com>
2018-08-05 20:56:57 -0400

    Add files via upload

---
+++ server/Server/WebServer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Linq;
+using System.Text;
+ 
+namespace Server
+{
+    public class WebServer
+    {
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly Func<HttpListenerRequest, string> _responderMethod;
+ 
+        public WebServer(Func<HttpListenerRequest, string> method)
+        {
+            if (!HttpListener.IsSupported)
+                throw new NotSupportedException(
+                    "Install System.Net.Http.");
+ 
+ 
+            if (method == null)
+                throw new ArgumentException("method");
+ 
+            _listener.Prefixes.Add("http://localhost:8081/");
+            _listener.Prefixes.Add("http://127.0.0.1:8081/");
+
+            _responderMethod = method;
+            _listener.Start();
+        }
+ 
+        public void Run()
+        {
+            ThreadPool.QueueUserWorkItem((o) =>
+            {
+                Console.WriteLine("Webserver running...");
+                try
+                {
+                    while (_listener.IsListening)
+                    {
+                        ThreadPool.QueueUserWorkItem((c) =>
+                        {
+                            var ctx = c as HttpListenerContext;
+                            try
+                            {
+                                string rstr = _responderMethod(ctx.Request);
+                                byte[] buf = Encoding.UTF8.GetBytes(rstr);
+                                ctx.Response.ContentLength64 = buf.Length;
+                                ctx.Response.OutputStream.Write(buf, 0, buf.Length);
+                            }
+                            catch { }
+                            finally
+                            {
+                                ctx.Response.OutputStream.Close();
+                            }
+                        }, _listener.GetContext());
+                    }
+                }
+                catch { }
+            });
+        }
+ 
+        public void Stop()
+        {
+            _listener.Stop();
+            _listener.Close();
+        }
+    }
+}
